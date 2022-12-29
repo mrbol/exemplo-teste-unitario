fdaeb5229c604d19d49438cea7bda11a68edc93e
tdr <tdr.evolution@gmail.com>
2022-12-29 10:34:05 -0300

    Exemplo basico de Testes Unitários com XUnit

---
+++ ExemploTesteAutomatizado/src/Bibliotecas/Calculo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecas
+{
+    public static class Calculo
+    {
+        public static double Somar(double numero1, double numero2)
+        {
+            try
+            {
+                return (numero1 + numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static double Subtrair(double numero1, double numero2)
+        {
+            try
+            {
+                return (numero1 - numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static double Multiplicar(double numero1, double numero2)
+        {
+            try
+            {
+                return (numero1 * numero2);
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static double Dividir(double numero1, double numero2)
+        {
+            try
+            {
+                return (numero1 / numero2);
+            }
+            catch (System.DivideByZeroException)
+            {
+                return -999;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool IsNumeroPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+    }
+}
