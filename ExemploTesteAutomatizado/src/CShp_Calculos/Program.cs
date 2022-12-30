a5c82a02eda10b25ea8b8b63bbbd01beceb97f50
tdr <tdr.evolution@gmail.com>
2022-12-30 18:29:47 -0300

    Utilizando a classe Calculo

--- ExemploTesteAutomatizado/src/CShp_Calculos/Program.cs
+++ ExemploTesteAutomatizado/src/CShp_Calculos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bibliotecas;
 
 namespace CShp_Calculos
 {
@@ -10,6 +11,12 @@ namespace CShp_Calculos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(string.Concat("Soma ", Calculo.Somar(1, 2)));
+            Console.WriteLine(string.Concat("Subtrair ", Calculo.Subtrair(2, 1)));
+            Console.WriteLine(string.Concat("Multiplicar ", Calculo.Multiplicar(2, 2)));
+            Console.WriteLine(string.Concat("Dividir ", Calculo.Dividir(4, 2)));
+            Console.Write("Pressione qualquer teclar para finalizar...");
+            Console.ReadKey();
         }
     }
 }
