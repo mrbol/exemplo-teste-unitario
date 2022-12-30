50512c6b46a2cbcf183bb8a4453d6527d46c0dc3
tdr <tdr.evolution@gmail.com>
2022-12-30 18:14:04 -0300

    uma classe bem simples com algumas operações matemáticas

--- ExemploTesteAutomatizado/src/Bibliotecas/Calculo.cs
+++ ExemploTesteAutomatizado/src/Bibliotecas/Calculo.cs
@@ -10,59 +10,19 @@ namespace Bibliotecas
     {
         public static double Somar(double numero1, double numero2)
         {
-            try
-            {
-                return (numero1 + numero2);
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
+            return (numero1 + numero2);
         }
-
         public static double Subtrair(double numero1, double numero2)
         {
-            try
-            {
-                return (numero1 - numero2);
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
+            return (numero1 - numero2);
         }
-
         public static double Multiplicar(double numero1, double numero2)
         {
-            try
-            {
-                return (numero1 * numero2);
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
+            return (numero1 * numero2);
         }
-
         public static double Dividir(double numero1, double numero2)
         {
-            try
-            {
-                return (numero1 / numero2);
-            }
-            catch (System.DivideByZeroException)
-            {
-                return -999;
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public static bool IsNumeroPar(int numero)
-        {
-            return numero % 2 == 0;
+            return (numero1 / numero2);
         }
     }
 }
